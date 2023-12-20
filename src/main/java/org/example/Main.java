6eeed8719e7ae18abb08f6b724713533d04b82d4
ElnurKasimov <ekasimov2012@gmail.com>
2023-12-20 22:46:10 +0200

    1. established connection with H2 2. created table COMPANY in database carsharing

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,38 @@
 package org.example;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String dbName = "";
+        if ("-databaseFileName".equals(args[0]) && "carsharing".equals(args[1])) {
+            dbName = args[1];
+        } else {
+            dbName = "someDatabase";
+        }
+
+        String url = "jdbc:h2:./src/carsharing/db/" + dbName;
+        try {
+            Class.forName ("org.h2.Driver");
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+        try (Connection con = DriverManager.getConnection(url);
+             Statement stat = con.createStatement() ){
+            con.setAutoCommit(true);
+            String createCompanyTable = "CREATE TABLE IF NOT EXISTS COMPANY (" +
+                    "ID INT," +
+                    "NAME VARCHAR(50));";
+            int res = stat.executeUpdate(createCompanyTable);
+            System.out.println(res);
+
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+
     }
 }
